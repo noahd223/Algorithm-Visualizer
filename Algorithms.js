ac6273dfbf19ee1c9fffe811af515f38f33f28f2
noahd223 <noahd3485@gmail.com>
2024-01-08 22:38:49 -0500

    Added button and changed from OOP design

--- Algorithms.js
+++ Algorithms.js
@@ -1,46 +1,38 @@
-class Algorithm {
-    #numOfBlocks = 0; // number of blocks to use
-    #type; // type of algorithm to display
-
-    constructor(numOfBlocks, type) {
-        this.#numOfBlocks = numOfBlocks;
-        this.#type = type;
+// Function to create blocks in the center of the screen 
+function createBlocks(container, numOfBlocks) {
+    for (var i = 0; i < numOfBlocks; i++) {
+        var block = document.createElement("div");
+        block.className = "block";
+        block.style.height = Math.floor((Math.random() * numOfBlocks * 4) + 5) + "px"; // Set random height for sorting
+        container.appendChild(block);
     }
+}
 
-    // Method to create blocks in the center of the screen
-    createBlocks(container) {
-        for (var i = 0; i < this.#numOfBlocks; i++) {
-            var block = document.createElement("div");
-            block.className = "block";
-            block.style.height = Math.floor((Math.random() * this.#numOfBlocks * 10) + 5) + "px"; // Set random height for sorting
-            container.appendChild(block);
-        }
+// Method to sort the blocks using insertion sort
+function insertionSort(container) {
+    var blocks = Array.from(container.children); // make it so it only adds the blocks to the array and not the buttons
+
+    for (let i = 1; i < blocks.length; i++) {
+        let currentIndex = i;
+
+        setTimeout(function () {
+            var currentBlock = blocks[currentIndex];
+            var previous = currentIndex - 1;
+
+            currentBlock.style.backgroundColor = "green";
+            while (previous >= 0 && parseInt(blocks[previous].style.height) > parseInt(currentBlock.style.height)) {
+                blocks[previous + 1] = blocks[previous];
+                previous = previous - 1;
+            }
+
+            blocks[previous + 1] = currentBlock;
+
+            currentBlock.style.backgroundColor = "#3498db"; // Reset background color after sorting
+            // Rearrange the sorted blocks in the container
+            container.innerHTML = "";
+            blocks.forEach(block => container.appendChild(block));
+        }, i * 250); // NEED TO IMPLEMENT FUNCTION TO MAKE TIME DECREASE AS BLOCKS INCREASE
     }
-    // Method to sort the blocks using insertion sort
-    insertionSort(container) {
-        var blocks = Array.from(container.children);
-
-        for (let i = 1; i < blocks.length; i++) {
-            let currentIndex = i;
-
-            setTimeout(function () {
-                var key = blocks[currentIndex];
-                var j = currentIndex - 1;
-
-                while (j >= 0 && parseInt(blocks[j].style.height) > parseInt(key.style.height)) {
-                    blocks[j + 1] = blocks[j];
-                    j = j - 1;
-                }
-
-                blocks[j + 1] = key;
-
-                // Rearrange the sorted blocks in the container
-                container.innerHTML = "";
-                blocks.forEach(block => container.appendChild(block));
-            }, i * 250); // NEED TO IMPLEMENT FUNCTION TO MAKE TIME DECREASE AS BLOCKS INCREASE
-        }
-    }
-
 }
 
 // function to draw the blocks on the screen based on the number the user requests
@@ -53,9 +45,15 @@ function drawBlocks() {
         drawBlocks();
     }
 
-    const algorithm = new Algorithm(numBlocks, "insertionSort");
-    algorithm.createBlocks(container);
-    algorithm.insertionSort(container);
+    const insertionSortButton = document.createElement("button");
+    insertionSortButton.textContent = "Insertion Sort";
+    document.body.appendChild(insertionSortButton);
+    createBlocks(container, numBlocks);
+    insertionSortButton.addEventListener("click", function () {
+        insertionSort(container);
+    });
+
+    
 }
 
 drawBlocks();
